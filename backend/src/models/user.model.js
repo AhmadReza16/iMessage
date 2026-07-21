38037926cbae6dfd0bb693612ea1e194a0925087
AhmadReza16git config  user,email <101175583+AhmadReza16@users.noreply.github.com>
2026-07-21 22:59:21 +0330

    create models

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose"
+
+
+const userSchema = new mongoose.Schema(
+    {
+    clerkId:{
+        type: String,
+        require:true,
+        unique: true,
+    },
+    email:{
+        type: String,
+        require:true,
+        unique: true,
+    },
+    fullName:{
+        type: String,
+        require:true,
+    },
+    profilePic:{
+        type: String,
+        default: "",
+    },
+  },
+    {timestamps:true}, //   created at or update at
+),
+
+const User = mongoose.model("User",userSchema);
+
+export default User;
